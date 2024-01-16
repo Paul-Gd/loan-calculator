88921fd7afae9ea36bc762c02acfaedff7c9e799
Paul Gordan <47354384+Paul-Gd@users.noreply.github.com>
2024-01-16 21:17:02 +0200

    Increase maxAssetSize

--- calculator/razzle.config.js
+++ calculator/razzle.config.js
@@ -5,7 +5,7 @@ module.exports = {
     return {
       ...webpackConfig,
       // chartjs has a bug so tree shaking does not work: https://github.com/chartjs/Chart.js/pull/10504
-      performance: { maxAssetSize: 500000, maxEntrypointSize: 500000 },
+      performance: { maxAssetSize: 1000000, maxEntrypointSize: 1000000 },
     };
   },
 };
